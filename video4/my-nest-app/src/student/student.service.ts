45a6cf094ab7fd20746f07ad2401efd6df2316d3
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2026-05-26 22:06:28 +0600

    Finished watching video10 to learn nestjs

---
+++ video4/my-nest-app/src/student/student.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class StudentService {
+    private students = [
+        { id: 1, name: 'John Doe', age: 20 },
+        { id: 2, name: 'Jane Smith', age: 22 },
+        { id: 3, name: 'Michael Johnson', age: 19 },
+    ];
+
+    getAllStudents() {
+        return this.students;
+    }
+
+    getStudentById(id: number) {
+        const student = this.students.find(student => student.id === id);
+        if (!student) {
+            throw new NotFoundException(`Student with id ${id} not found`);
+        }
+        return student;
+    }
+
+    // POST
+    createStudent(data: { name: string, age: number }) {
+        const newStudent = {
+            id: Date.now(), // Generate a unique ID based on the current timestamp
+            ...data
+        };
+        this.students.push(newStudent);
+        return newStudent;
+    }
+
+    // PUT
+    updateStudent(id: number, data: { name: string, age: number }) {
+        const index = this.students.findIndex((student) => student.id === id);
+
+        if (index === -1) {
+            throw new NotFoundException(`Student with id ${id} not found`);
+        }
+
+        this.students[index] = { id, ...data };
+        return this.students[index];
+    }
+
+    // PATCH
+    patchStudent(id: number, data: Partial<{ name: string; age: number }>) {
+        const student = this.getStudentById(id);
+        // const student = this.students.find(student => student.id === id);
+
+        Object.assign(student, data);
+        return student;
+    }
+
+    // DELETE
+    deleteStudent(id: number) {
+        const index = this.students.findIndex(student => student.id === id);
+        if (index === -1) {
+            throw new NotFoundException(`Student with id ${id} not found`);
+        }
+        const deletedStudent = this.students.splice(index, 1);
+        return { message: `Student with id ${id} deleted successfully`, student: deletedStudent[0] };
+    }
+}
